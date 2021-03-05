295630ca670c137cdc2c9f7af6702c935f6a5608
Ishan Gogna <ig2230@nyu.edu>
2021-03-05 16:46:28 -0500

    added comments

--- remembering-value.scm
+++ remembering-value.scm
@@ -5,3 +5,18 @@
 
 (define add-3(make-add-x 3))
 (display (add-3 4))
+
+; def makeAddX(x):
+    
+;     def addX(y):
+;         print(x+y)
+    
+;     return addX
+    
+
+; my_func = makeAddX(3)
+; my_func(4)
+
+; This is similar to the python code above. 
+; Note that EVEN AFTER THE MAKEADDX FUNCTION WAS EXECUTED COMPLETELY LINE 17, ADDX STILL REMEMBERS THAT THE VALUE OF X IS 3!
+; This is a closure.
